f0c072f1b2b0794c4d3f87bb5b05da15940222c2
galaxy-captain <goskkyer@gmail.com>
2017-02-14 15:23:34 +0800

    test code

---
+++ src/test/java/Server.java
@@ -0,0 +1,14 @@
+import org.goskyer.microserver.bio.MicroServer;
+
+/**
+ * Created by galaxy on 2017/2/14.
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        MicroServer server = new MicroServer();
+        server.initServer();
+        server.startServer();
+    }
+
+}
